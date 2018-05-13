1cfd33585506aada75e58afaa779a999b4321e7b
loujialu <39003907+loujialu@users.noreply.github.com>
2018-05-13 19:26:32 +0800

    ok
    ok

---
+++ js/index.js
@@ -0,0 +1,77 @@
+window.onload = function () {
+    search();
+    secondKill();
+};
+
+var search = function(){
+    var search = document.getElementsByClassName('jd_header_box')[0];
+    var banner = document.getElementsByClassName('jd_banner')[0];
+
+    var height = banner.offsetHeight;
+
+    window.onscroll = function () {
+        var top = document.body.scrollTop;
+
+        if(top > height){
+            search.style.background = "rgba(201,21,35,0.85)";
+        }else{
+            var op = top/height * 0.85
+            search.style.background = "rgba(201,21,35,"+op+")";
+        }
+    }
+}
+
+var imgbox = document.getElementById("imgbox");
+var imgboxWidth = imgbox.offsetWidth / 10;
+console.log(imgboxWidth);
+var num = 0;
+
+function autoplay() {
+    if (num === -imgboxWidth * 8) {
+        num = 0;
+    }
+    num = num - 1;
+    imgbox.style.left = num + "px";
+    if (num % imgboxWidth === 0) {
+        clearInterval(timer1);
+        timer2 = setTimeout(function () {
+            timer1 = setInterval("autoplay()", 1);
+        }, 3000);
+    }
+}
+var timer1 = setInterval("autoplay()", 1);
+
+/*秒杀倒计时*/
+var secondKill = function(){
+    /*复盒子*/
+    var parentTime = document.getElementsByClassName('sk_time')[0];
+    /*span时间*/
+    var timeList = parentTime.getElementsByClassName('num');
+
+    console.log(timeList.length);
+
+    var times = 7   * 60 * 60;
+    var timer;
+    timer = setInterval(function(){
+        times  -- ;
+
+        var h = Math.floor(times/(60*60));
+        var m = Math.floor(times/60%60);
+        var s = times%60;
+
+        console.log(h+'-'+m+"-"+s);
+
+        timeList[0].innerHTML = h>=10?Math.floor(h/10):0;
+        timeList[1].innerHTML = h%10;
+
+        timeList[2].innerHTML = m>=10?Math.floor(m/10):0;
+        timeList[3].innerHTML = m%10;
+
+        timeList[4].innerHTML = s>=10?Math.floor(s/10):0;
+        timeList[5].innerHTML = s%10;
+        if(times <= 0){
+            clearInterval(timer);
+        }
+    },1000);
+
+}
